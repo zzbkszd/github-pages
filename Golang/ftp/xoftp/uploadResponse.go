b65f993ec6be2eee858031b078c1cc9baa7e828e
Shi Zhida <zzbkszd@163.com>
2016-10-17 17:44:18 +0800

    文件服务器

---
+++ Golang/ftp/xoftp/uploadResponse.go
@@ -0,0 +1,39 @@
+package xoftp
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+//UploadResponse 返回消息
+type UploadResponse struct {
+	State int    `json:"state"`
+	URL   string `json:"data"`
+	Msg   string `json:"msg"`
+}
+
+//SendJsonp 发送jsonp消息
+func (ur *UploadResponse) SendJsonp(w http.ResponseWriter) {
+	fmt.Println("send message msg:", ur.Msg, " state : ", ur.State)
+
+	doc, err := json.Marshal(ur)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	jsonp := "xoCallback(" + string(doc) + ")"
+	fmt.Println(jsonp)
+	fmt.Fprintf(w, jsonp)
+}
+
+//Send 发送消息
+func (ur *UploadResponse) Send(w http.ResponseWriter) {
+	fmt.Println("send message msg:", ur.Msg, " state : ", ur.State)
+
+	doc, err := json.Marshal(ur)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(string(doc))
+	fmt.Fprintf(w, string(doc))
+}
